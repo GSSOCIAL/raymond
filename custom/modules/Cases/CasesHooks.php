66541bac6fb6f91f02c1fe7c6305aa28f1db1e3a
Timur Rakhimzhanov <t@timur.pro>
2017-06-08 15:57:34 +0300

    Автоматическая смена статуса
    https://trello.com/c/QFVmryCE/54-автоматическая-смена-статуса
    
    [+] Добавил logic_hook для смены статуса при изменении полей case’a и изменения связей

---
+++ custom/modules/Cases/CasesHooks.php
@@ -0,0 +1,33 @@
+<?php
+
+class CasesHooks {
+
+    function before_save(&$bean, $event, $arguments) { 
+        if ( !empty($bean->fetched_row['id']) /*&& CasesHooks::$afterChangeStatus == false */) {
+            $fields = $bean->getFieldDefinitions();
+            $changed = false;
+            foreach ( $fields as $field => $def ) {
+                if ( $def['source'] != 'non-db' && !in_array($field, array('date_modified', 'date_entered')) && $bean->$field != $bean->fetched_row[$field] ) {
+                    $changed = true; 
+                    break;
+                }
+            }
+            if ( $changed ) {
+                if ( $bean->status == 'Open_New' ) {
+                    $bean->status = 'Open_Assigned';
+                }
+            }
+        }
+    }
+
+    function after_relationship_add(&$bean, $event, $arguments) {
+        if ( !empty($bean->fetched_row['id']) && $bean->status == 'Open_New' /*&& CasesHooks::$afterChangeStatus == false*/) {
+            if ( $bean->fetched_row['account_id'] != $arguments['related_id'] || $arguments['related_module'] != 'Accounts' ) {
+                CasesHooks::$afterChangeStatus == true; 
+                $bean->status = 'Open_Assigned';
+                $bean->save();
+            }
+        }
+    }
+
+}
