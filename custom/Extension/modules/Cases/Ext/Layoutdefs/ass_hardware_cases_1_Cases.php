317d708bf2307e7f80354b5f818d6f85d38a6c0d
toxicaria <iwealth@yandex.ru>
2017-12-21 17:08:46 +0300

    hardware page from cases
    https://trello.com/c/jVil5jqB/74-hardware-page-from-cases

--- custom/Extension/modules/Cases/Ext/Layoutdefs/ass_hardware_cases_1_Cases.php
+++ custom/Extension/modules/Cases/Ext/Layoutdefs/ass_hardware_cases_1_Cases.php
@@ -3,7 +3,7 @@
 $layout_defs["Cases"]["subpanel_setup"]['ass_hardware_cases_1'] = array (
   'order' => 100,
   'module' => 'ass_hardware',
-  'subpanel_name' => 'default',
+  'subpanel_name' => 'ForCases',
   'sort_order' => 'asc',
   'sort_by' => 'id',
   'title_key' => 'LBL_ASS_HARDWARE_CASES_1_FROM_ASS_HARDWARE_TITLE',
@@ -12,11 +12,11 @@ $layout_defs["Cases"]["subpanel_setup"]['ass_hardware_cases_1'] = array (
   array (
     0 => 
     array (
-      'widget_class' => 'SubPanelTopButtonQuickCreate',
+      'widget_class' => 'SubPanelTopButtonQuickCreateHardware',
     ),
     1 => 
     array (
-      'widget_class' => 'SubPanelTopSelectButton',
+      'widget_class' => 'SubPanelTopSelectButtonHardware',
       'mode' => 'MultiSelect',
     ),
   ),
