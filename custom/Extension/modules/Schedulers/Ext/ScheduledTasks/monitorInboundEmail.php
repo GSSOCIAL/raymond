105a7fae84f4de2b78a848c66e6ed5f2501559d0
GSSSOCIAL <gsssocial@icloud.com>
2019-08-27 16:07:24 +0300

    Item 63 - Monitor that inbound email working: Task: https://trello.com/c/ZSsv4opE

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/monitorInboundEmail.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/monitorInboundEmail.php
@@ -1,7 +1,4 @@
 <?php 
- //WARNING: The contents of this file are auto-generated
-
- 
 /*
 https://trello.com/c/ZSsv4opE
 Check if Inbound Emails works. Send and compare emails for spec keys
@@ -15,13 +12,19 @@ function monitorInboundEmail(){
     global $db;
     $InboundSystemAddressPrefs = getEmailVerifierAddr(true);
     $Subject = "Inbound Email Verification"; //Letter subject. We will compare incoming mail subjects with this one
-    $DDATA = array(
+    $errors = array(
         "mailbox_access"=>false,
-        "has_unread_messages"=>false
+        "has_unread_messages"=>false,
+        "case_updates"=>array(),
+        "errors"=>array(),  
     );
     $KeysTableExist = $db->query("DESCRIBE `verification_keys`");
-    $DDATA["table_exist"] = $KeysTableExist !== false;
+    $errors["table_exist"] = $KeysTableExist !== false;
+    if($errors["table_exist"]==false){
+        $errors["errors"][] = "Couldnt save Verification keys. Table doesnt exists";
+    }
     $VerificationPassed=false;
+    //If system address exists
     if($InboundSystemAddressPrefs && $InboundSystemAddressPrefs->addr){
         //Address exists. Check emails
         require_once 'modules/InboundEmail/AOPInboundEmail.php';
@@ -43,19 +46,18 @@ function monitorInboundEmail(){
             $connectToMailServer = false;
             if($ieX->isPop3Protocol()){
                 $msgNoToUIDL = $ieX->getPop3NewMessagesToDownloadForCron();
-                // get all the keys which are msgnos;
                 $newMsgs = array_keys($msgNoToUIDL);
             }
             if($ieX->connectMailserver() == 'true'){
                 $connectToMailServer = true;
             }
             if($connectToMailServer){
-                $DDATA["mailbox_access"]=true;
+                $errors["mailbox_access"]=true;
                 if(!$ieX->isPop3Protocol()) {
                     $newMsgs = $ieX->getNewMessageIds();
                 }
                 if(is_array($newMsgs)){
-                    $DDATA["has_unread_messages"]=true;
+                    $errors["has_unread_messages"]=true;
                     foreach ($newMsgs as $k => $msgNo){
                         //Filter each email by subject
                         $header = imap_headerinfo($ieX->conn,$msgNo);
@@ -85,6 +87,7 @@ function monitorInboundEmail(){
                 }
             }else{
                 $GLOBALS["log"]->error("Monitor Inbound emails failed. Not connected.");
+                $errors["errors"][] = "Failure: couldnt connect to mailbox";
             }
         }
         imap_expunge($ieX->conn);
@@ -93,6 +96,7 @@ function monitorInboundEmail(){
         $code = generateCode();
         if(!$db->query(sprintf("INSERT INTO `verification_keys`(`code`,`thru`) VALUES ('%s','%s')",md5($code),date("Y-m-d H:i:s",strtotime("+3 hours"))))){
             $GLOBALS["log"]->error("INBOUND EMAIL VERIFY Error - Couldnt save key");
+            $errors["errors"][] = "Couldnt save key";
         }
         //Key reserved. Send mail to System addr
         $Email = new Email();  
@@ -108,10 +112,12 @@ function monitorInboundEmail(){
         @$mail->Send();
     }else{
         $GLOBALS["log"]->error("Monitor Inbound emails failed. Address failed");
+        $errors["errors"][] = "E-mail address failure: Couldnt get email address to verify.";
     }
     if(!$VerificationPassed){
         //Write to log
         $GLOBALS["log"]->error("Monitor Inbound emails failed.  Couldnt verify code");
+        $errors["errors"][] = "Couldnt verify code.";
         //Send email
         if($naddress = getEmailNotifyAddr()){
             $Email = new Email();  
@@ -121,18 +127,53 @@ function monitorInboundEmail(){
             $system_mail->From = $defaults['email'];  
             $system_mail->FromName = $defaults['name'];  
             $system_mail->Subject = "Inbound mailboxes error. Couldnt verify code";
-            $Body = "System cant verify mailboxes. Please check that email service works and contact administrator.\n\nDebug data:\n";
-            $Body .= "email account: ".(!empty($InboundSystemAddressPrefs)?$InboundSystemAddressPrefs->addr . " ({$InboundSystemAddressPrefs->id})":" EMPTY")."\n";
-            $Body .= "mailbox access: ".($DDATA["mailbox_access"]===true?"y":"n")."\n";
-            $Body .= "unread messages: ".($DDATA["has_unread_messages"]===true?"y":"n")."\n";
-            $Body .= "key table exists: ".($DDATA["table_exist"]===true?"y":"n")."\n";
+            $Body = "System cant verify mailboxes. Please check that email service works and contact administrator.\n\n";
+            
+            if(!empty($errors["errors"])){
+                $errors_list = "";
+                foreach($errors["errors"] as $error){
+                    $errors_list .= $error.";<br/>";
+                }
+                $Body .= "Errors:\n{$errors_list}\n\n";
+            }
+            $Body .= "Debug data:\nemail account: ".(!empty($InboundSystemAddressPrefs)?$InboundSystemAddressPrefs->addr . " ({$InboundSystemAddressPrefs->id})":" EMPTY")."\n";
+            $Body .= "mailbox access: ".($errors["mailbox_access"]===true?"y":"n")."\n";
+            $Body .= "unread messages: ".($errors["has_unread_messages"]===true?"y":"n")."\n";
+            $Body .= "key table exists: ".($errors["table_exist"]===true?"y":"n")."\n";
             $system_mail->Body = $Body;
             $system_mail->prepForOutbound();  
             $system_mail->AddAddress(getEmailNotifyAddr());  
             @$system_mail->Send();
         }
-        //Create Task
-        
+        //Update debug task
+        $message = date("Y-m-d").": ";
+        if(!empty($errors["errors"])){
+            $message .= implode(",",$errors["errors"]);
+        }else{
+            $message .= "Couldnt save code.";
+            if(!$errors["mailbox_access"]){
+                $message .= "Couldnt access mailbox.";
+            }
+            if(!$errors["table_exist"]){
+                $message .= "Storage table doesnt exists.";
+            }
+        }
+        if($task_id = $db->getOne("SELECT t.id FROM config c INNER JOIN tasks t ON t.id=c.value WHERE c.name='email_debug_task' AND c.category='system' AND t.deleted=0")){
+            $Task = BeanFactory::getBean("Tasks",$task_id);
+            $Task->description = "{$Task->description}\n{$message}";
+            $Task->save();
+        }else{
+            $Task = new Task();
+            $Task->name = "Email verify errors";
+            $Task->status = "Pending Input";
+            $Task->description = $message;
+            $Task->save();
+
+            //save config
+            $Administration = new Administration();
+            $Administration->saveSetting("system","email_debug_task",$Task->id);
+            $Administration->saveConfig();
+        }
         echo("Email settings fault to verify. Please contact administrator");
     }else{
         echo("Email settings verified success");
@@ -141,5 +182,4 @@ function monitorInboundEmail(){
     $db->query("UPDATE `verification_keys` SET `activated`='2' WHERE `activated`='0' AND `bean` IS NOT NULL");
     return true;
 }
-
 ?>
