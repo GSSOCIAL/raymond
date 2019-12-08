1102bc69e54019414883cafcc7ed65c429859807
GSSSOCIAL <gsssocial@icloud.com>
2019-12-08 18:53:23 +0200

    DCMSYS-18: Fixed .docs file

--- custom/include/beanExport.php
+++ custom/include/beanExport.php
@@ -257,7 +257,7 @@ class BeanExport{
         //Export document
         if($this->force_download===true){
             header("Content-Description: File Transfer");
-            header("Content-Disposition: attachment; filename={$this->object_name}_{$this->record_id}.docx");
+            header("Content-Disposition: attachment; filename={$this->filename}.docx");
             header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
             header("Content-Transfer-Encoding: binary");
             header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
@@ -267,7 +267,7 @@ class BeanExport{
             exit();
         }
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-        $objWriter->save('helloWorld.docx');
+        $objWriter->save("{$this->filename}.docx");
         $ctx="";
         return $ctx;
     }
@@ -341,10 +341,29 @@ class BeanExport{
                 }
             break;
             case "string":
+                $body = htmlspecialchars_decode($body);
+
                 $table = $node->addTable();
                 $table->addRow();
                 $table->addCell()->addText($name,array("bold"=>true));
-                $table->addCell()->addText(htmlspecialchars($body));
+
+                /*
+                This code will remove tags
+                $body = preg_replace("/<br\s\/>/","\n",$body);
+                $body = strip_tags($body,"<a>");
+                $body = preg_replace_callback('/<a.+?href=[\'"](.+)[\'"](.+)?>(.+)<\/a>/',function($m){
+                    return $m[1];
+                },$body);
+                $body = strip_tags($body);*/
+                
+                $body = htmlentities($body);
+
+                $xml = array('&#34;','&#38;','&#38;','&#60;','&#62;','&#160;','&#161;','&#162;','&#163;','&#164;','&#165;','&#166;','&#167;','&#168;','&#169;','&#170;','&#171;','&#172;','&#173;','&#174;','&#175;','&#176;','&#177;','&#178;','&#179;','&#180;','&#181;','&#182;','&#183;','&#184;','&#185;','&#186;','&#187;','&#188;','&#189;','&#190;','&#191;','&#192;','&#193;','&#194;','&#195;','&#196;','&#197;','&#198;','&#199;','&#200;','&#201;','&#202;','&#203;','&#204;','&#205;','&#206;','&#207;','&#208;','&#209;','&#210;','&#211;','&#212;','&#213;','&#214;','&#215;','&#216;','&#217;','&#218;','&#219;','&#220;','&#221;','&#222;','&#223;','&#224;','&#225;','&#226;','&#227;','&#228;','&#229;','&#230;','&#231;','&#232;','&#233;','&#234;','&#235;','&#236;','&#237;','&#238;','&#239;','&#240;','&#241;','&#242;','&#243;','&#244;','&#245;','&#246;','&#247;','&#248;','&#249;','&#250;','&#251;','&#252;','&#253;','&#254;','&#255;');
+                $html = array('&quot;','&amp;','&amp;','&lt;','&gt;','&nbsp;','&iexcl;','&cent;','&pound;','&curren;','&yen;','&brvbar;','&sect;','&uml;','&copy;','&ordf;','&laquo;','&not;','&shy;','&reg;','&macr;','&deg;','&plusmn;','&sup2;','&sup3;','&acute;','&micro;','&para;','&middot;','&cedil;','&sup1;','&ordm;','&raquo;','&frac14;','&frac12;','&frac34;','&iquest;','&Agrave;','&Aacute;','&Acirc;','&Atilde;','&Auml;','&Aring;','&AElig;','&Ccedil;','&Egrave;','&Eacute;','&Ecirc;','&Euml;','&Igrave;','&Iacute;','&Icirc;','&Iuml;','&ETH;','&Ntilde;','&Ograve;','&Oacute;','&Ocirc;','&Otilde;','&Ouml;','&times;','&Oslash;','&Ugrave;','&Uacute;','&Ucirc;','&Uuml;','&Yacute;','&THORN;','&szlig;','&agrave;','&aacute;','&acirc;','&atilde;','&auml;','&aring;','&aelig;','&ccedil;','&egrave;','&eacute;','&ecirc;','&euml;','&igrave;','&iacute;','&icirc;','&iuml;','&eth;','&ntilde;','&ograve;','&oacute;','&ocirc;','&otilde;','&ouml;','&divide;','&oslash;','&ugrave;','&uacute;','&ucirc;','&uuml;','&yacute;','&thorn;','&yuml;');
+                $body = str_replace($html,$xml,$body);
+                $body = str_ireplace($html,$xml,$body);
+
+                $table->addCell()->addText($body);
             break;
         }
         return $node;
