985cfd851682e1cacebb40467d3349435515a59e
GSSSOCIAL <gsssocial@icloud.com>
2020-01-07 21:28:09 +0200

    DCMSYS-16: Убраны кнопки

--- custom/modules/ass_hardware/metadata/detailviewdefs.php
+++ custom/modules/ass_hardware/metadata/detailviewdefs.php
@@ -13,16 +13,7 @@ array (
           0 => 'EDIT',
           1 => 'DUPLICATE',
           2 => 'DELETE',
-          3 => 'FIND_DUPLICATES',
-          4 => array(
-            'customCode' => '<input type="submit" class="button" title="{$MOD.LBL_CONVERT_TO_XML}" onclick="window.bean_server.export(\'xml\');return false;" value="{$MOD.LBL_CONVERT_TO_XML_BUTTON}"/>',
-          ),
-          5 => array(
-            'customCode' => '<input type="submit" class="button" title="{$MOD.LBL_CONVERT_TO_HTML}" onclick="window.bean_server.export(\'html\');return false;" value="{$MOD.LBL_CONVERT_TO_HTML_BUTTON}"/>',
-          ),
-          6 => array(
-            'customCode' => '<input type="submit" class="button" title="{$MOD.LBL_CONVERT_TO_DOCX}" onclick="window.bean_server.export(\'docx\');return false;" value="{$MOD.LBL_CONVERT_TO_DOCX_BUTTON}"/>',
-          ),
+          3 => 'FIND_DUPLICATES'
         ),
       ),
       'maxColumns' => '2',
@@ -40,9 +31,6 @@ array (
         ),
       ),
       'includes' => array(
-        0 => array (
-          'file' => 'custom/include/js/bean_export.js',
-        ),
       ),
       'useTabs' => true,
       'tabDefs' => 
